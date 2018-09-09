0a90252abd380ec7a305ce1441994121aea26b9a
Alejandro Ojeda <alex@x3y.org>
2018-09-09 20:01:08 +0200

    Correccion

--- src/main/java/utilities/JspViewUtils.java
+++ src/main/java/utilities/JspViewUtils.java
@@ -57,7 +57,7 @@ public class JspViewUtils {
             }
         }
 
-        if (paramValue != null && paramValue.isEmpty()) {
+        if (paramValue != null && !paramValue.isEmpty()) {
             if (first) {
                 first = false;
                 result.append("?");
